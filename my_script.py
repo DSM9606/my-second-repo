d573c22b1407aaa9bfd0326cc8acc9bfd10ef201
Dan Martin <66933628+DSM9606@users.noreply.github.com>
2021-05-29 22:23:42 -0400

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -16,6 +16,227 @@ print("Mission Success Systems Disabled")
 
 print("Critical Support Systems Disabled")
 
+print("Critical Error Abort")
+
+print("Hard System Reset")
+
+print("Mission Critical Systems Disabled")
+
+print("Defense Critical Systems Disabled")
+
+print("Mission Ready Systems Disabled")
+
+print("Mission Limited Systems Disabled")
+
+print("Mission Success Systems Disabled")
+
+print("Critical Support Systems Disabled")
+
+print("Downloading...")
+
+print("Critical Error Abort")
+
+print("Hard System Reset")
+
+print("Mission Critical Systems Disabled")
+
+print("Defense Critical Systems Disabled")
+
+print("Mission Ready Systems Disabled")
+
+print("Mission Limited Systems Disabled")
+
+print("Mission Success Systems Disabled")
+
+print("Critical Support Systems Disabled")
+
+print("Critical Error Abort")
+
+print("Hard System Reset")
+
+print("Mission Critical Systems Disabled")
+
+print("Defense Critical Systems Disabled")
+
+print("Mission Ready Systems Disabled")
+
+print("Mission Limited Systems Disabled")
+
+print("Mission Success Systems Disabled")
+
+print("Critical Support Systems Disabled")
+
+print("Downloading...")
+
+print("Critical Error Abort")
+
+print("Hard System Reset")
+
+print("Mission Critical Systems Disabled")
+
+print("Defense Critical Systems Disabled")
+
+print("Mission Ready Systems Disabled")
+
+print("Mission Limited Systems Disabled")
+
+print("Mission Success Systems Disabled")
+
+print("Critical Support Systems Disabled")
+
+print("Critical Error Abort")
+
+print("Hard System Reset")
+
+print("Mission Critical Systems Disabled")
+
+print("Defense Critical Systems Disabled")
+
+print("Mission Ready Systems Disabled")
+
+print("Mission Limited Systems Disabled")
+
+print("Mission Success Systems Disabled")
+
+print("Critical Support Systems Disabled")
+
+print("Downloading...")
+
+print("Critical Error Abort")
+
+print("Hard System Reset")
+
+print("Mission Critical Systems Disabled")
+
+print("Defense Critical Systems Disabled")
+
+print("Mission Ready Systems Disabled")
+
+print("Mission Limited Systems Disabled")
+
+print("Mission Success Systems Disabled")
+
+print("Critical Support Systems Disabled")
+
+print("Critical Error Abort")
+
+print("Hard System Reset")
+
+print("Mission Critical Systems Disabled")
+
+print("Defense Critical Systems Disabled")
+
+print("Mission Ready Systems Disabled")
+
+print("Mission Limited Systems Disabled")
+
+print("Mission Success Systems Disabled")
+
+print("Critical Support Systems Disabled")
+
+print("Downloading...")
+
+print("Critical Error Abort")
+
+print("Hard System Reset")
+
+print("Mission Critical Systems Disabled")
+
+print("Defense Critical Systems Disabled")
+
+print("Mission Ready Systems Disabled")
+
+print("Mission Limited Systems Disabled")
+
+print("Mission Success Systems Disabled")
+
+print("Critical Support Systems Disabled")
+
+print("Critical Error Abort")
+
+print("Hard System Reset")
+
+print("Mission Critical Systems Disabled")
+
+print("Defense Critical Systems Disabled")
+
+print("Mission Ready Systems Disabled")
+
+print("Mission Limited Systems Disabled")
+
+print("Mission Success Systems Disabled")
+
+print("Critical Support Systems Disabled")
+
+print("Downloading...")
+
+print("Critical Error Abort")
+
+print("Hard System Reset")
+
+print("Mission Critical Systems Disabled")
+
+print("Defense Critical Systems Disabled")
+
+print("Mission Ready Systems Disabled")
+
+print("Mission Limited Systems Disabled")
+
+print("Mission Success Systems Disabled")
+
+print("Critical Support Systems Disabled")
+
+print("Critical Error Abort")
+
+print("Hard System Reset")
+
+print("Mission Critical Systems Disabled")
+
+print("Defense Critical Systems Disabled")
+
+print("Mission Ready Systems Disabled")
+
+print("Mission Limited Systems Disabled")
+
+print("Mission Success Systems Disabled")
+
+print("Critical Support Systems Disabled")
+
+print("Downloading...")
+
+print("Critical Error Abort")
+
+print("Hard System Reset")
+
+print("Mission Critical Systems Disabled")
+
+print("Defense Critical Systems Disabled")
+
+print("Mission Ready Systems Disabled")
+
+print("Mission Limited Systems Disabled")
+
+print("Mission Success Systems Disabled")
+
+print("Critical Support Systems Disabled")
+
+print("Critical Error Abort")
+
+print("Hard System Reset")
+
+print("Mission Critical Systems Disabled")
+
+print("Defense Critical Systems Disabled")
+
+print("Mission Ready Systems Disabled")
+
+print("Mission Limited Systems Disabled")
+
+print("Mission Success Systems Disabled")
+
+print("Critical Support Systems Disabled")
+
+
 x = 2 + 2
 print(x)
 
