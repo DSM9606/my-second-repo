e0288ad93348423c19af4a847d4928d044134976
Dan Martin <66933628+DSM9606@users.noreply.github.com>
2021-05-29 22:21:29 -0400

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -1,4 +1,20 @@
-print("HELLO WORLD!")
+print("Downloading...")
+
+print("Critical Error Abort")
+
+print("Hard System Reset")
+
+print("Mission Critical Systems Disabled")
+
+print("Defense Critical Systems Disabled")
+
+print("Mission Ready Systems Disabled")
+
+print("Mission Limited Systems Disabled")
+
+print("Mission Success Systems Disabled")
+
+print("Critical Support Systems Disabled")
 
 x = 2 + 2
 print(x)
